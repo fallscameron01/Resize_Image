132f66dd5a4efcee403504a6367fda7569de994f
Cameron Falls <camfalls0201@gmail.com>
2020-04-04 14:52:03 -0500

    Initial documentation and imports

--- resize.py
+++ resize.py
@@ -1,8 +1,21 @@
 """
+Contains the resize function that will resize an image stored in an ndarray by a scaling factor.
 """
+
+import numpy as np
 
 def resize(image, factor):
     """
+    Function to resize images by a scaling factor.
+
+    Parameters
+    ----------
+    image - ndarray - the image to resize
+    factor - int - scaling factor to resize by
+
+    Returns
+    -------
+    ndarray - the resized image
     """
     pass
 
